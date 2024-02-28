df79b51af4966af1f71422ec4d6491011e9b0ce5
Danil-Dang <dinodanildang@gmail.com>
2024-02-28 17:38:01 +0700

    Added Angular Client and Web Api
    Initial directories

---
+++ Web Api/Users/Users/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Users.Entities
+{
+	public class User
+	{
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
