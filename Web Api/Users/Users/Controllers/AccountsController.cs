94d13ba379494ecd3975a7d237d4441c940f271a
Danil-Dang <dinodanildang@gmail.com>
2024-03-05 09:36:21 +0700

    Registration DTO + ApiController + IMapper
    1) UserRegistrationDto; 2) UserRegistrationResponseDto; 3) Created MappingProfile; 4) AccountsController - RegisterUser (inject UserManager class + IMapper interface)

---
+++ Web Api/Users/Users/Controllers/AccountsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Users.Entities.Models;
+using Users.Entities.Dto;
+
+namespace Users.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public AccountsController(UserManager<User> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost("Registration")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegistrationDto userRegistration)
+        {
+            if (userRegistration == null || !ModelState.IsValid)
+                return BadRequest();
+
+            var user = _mapper.Map<User>(userRegistration);
+            var result = await _userManager.CreateAsync(user, userRegistration.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+
+                return BadRequest(new UserRegistrationResponseDto { Errors = errors });
+            }
+
+            return StatusCode(201);
+        }
+    }
+}
+
---
+++ Web Api/Users/Users/Entities/Dto/UserRegistrationResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Users.Entities.Dto
+{
+	public class UserRegistrationResponseDto
+	{
+        public bool IsSuccessfulRegistration { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
+
