c8ef8f537ff02befd9ee0b5d78f1274fd32ae80b
Danil-Dang <dinodanildang@gmail.com>
2024-03-18 14:56:36 +0700

    StorageService update-1

--- AngularClient/src/app/_services/storage.service.ts
+++ AngularClient/src/app/_services/storage.service.ts
@@ -11,19 +11,15 @@ export class StorageService {
   constructor() { }
 
   clean(): void {
-    // window.sessionStorage.clear();
     window.localStorage.clear();
   }
 
   public saveUser(user: any): void {
-    // window.sessionStorage.removeItem(USER_KEY);
-    // window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
     window.localStorage.removeItem(USER_KEY);
     window.localStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
   public getUser(): any {
-    // const user = window.sessionStorage.getItem(USER_KEY);
     const user = window.localStorage.getItem(USER_KEY);
     if (user) {
       return JSON.parse(user);
@@ -32,7 +28,6 @@ export class StorageService {
   }
 
   public isLoggedIn(): boolean {
-    // const user = window.sessionStorage.getItem(USER_KEY);
     const user = window.localStorage.getItem(USER_KEY);
     if (user) {
       return true;
@@ -41,14 +36,15 @@ export class StorageService {
   }
 
   public saveToken(token: string): void {
-    // window.sessionStorage.removeItem(TOKEN_KEY);
-    // window.sessionStorage.setItem(TOKEN_KEY, token);
     window.localStorage.removeItem(TOKEN_KEY);
     window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string | null {
-    // return window.sessionStorage.getItem(TOKEN_KEY);
-    return window.localStorage.getItem(TOKEN_KEY);
+    const token = window.localStorage.getItem(TOKEN_KEY);
+    if (token) {
+      return token;
+    }
+    return '';
   }
 }
