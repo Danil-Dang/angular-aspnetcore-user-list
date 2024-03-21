669bd0dec9299e12f709320dbdbe2cab2ffafae2
Danil-Dang <dinodanildang@gmail.com>
2024-03-21 09:08:40 +0700

    Registration

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
@@ -10,7 +11,7 @@ const routes: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  // { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
   
   { path: 'profile', component: ProfileComponent },
   // { path: 'user', component: BoardUserComponent },
--- AngularClient/src/app/register/register.component.ts
+++ AngularClient/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form: any = {
+    firstName: null,
+    lastName: null,
+    username: null,
+    email: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
+  constructor(
+    private authService: AuthService,
+    private _router: Router,
+    private storageService: StorageService,
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if (this.isLoggedIn) { this._router.navigate(["/home"]); }
+  }
+
+  onSubmit(): void {
+    const { firstName, lastName, username, email, password } = this.form;
+
+    this.authService.register({firstName, lastName, username, email, password}).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
 }
