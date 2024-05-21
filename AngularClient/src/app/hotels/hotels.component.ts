b7a051e41b50cc72ef4cf79e484342258c99f171
Danil-Dang <dinodanildang@gmail.com>
2024-05-21 10:38:51 +0700

    Hotel page design

--- AngularClient/src/app/hotels/hotels.component.ts
+++ AngularClient/src/app/hotels/hotels.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { ListHotel } from '../manager-user/list-hotel';
+import { ListService } from '../_services/list.service';
+import { DataService } from '../_services/data.service';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrl: './hotels.component.css'
+  styleUrl: './hotels.component.css',
 })
-export class HotelsComponent {
+export class HotelsComponent implements OnInit {
+  loggedIn: boolean;
+  currentUser: any;
+  isLoggedIn = false;
 
+  hoteLists$: Observable<ListHotel[]> = new Observable();
+  hotelId?: number;
+
+  constructor(
+    private listsService: ListService,
+    private storageService: StorageService,
+    private _router: Router,
+    private dataService: DataService // private fb: FormBuilder,
+  ) {
+    this.loggedIn = false;
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if (!this.isLoggedIn) {
+      this._router.navigate(['/home']);
+    }
+
+    this.currentUser = this.storageService.getUser();
+    this.fetchLists();
+  }
+
+  private fetchLists(): void {
+    this.hoteLists$ = this.listsService.getHotelLists();
+  }
+
+  generateStars(ratingValue: number): any[] {
+    const stars = [];
+    for (let i = 0; i < ratingValue; i++) {
+      stars.push({}); // Placeholder for each star element
+    }
+    return stars;
+  }
 }
